ba4d75e6873f18333fe5a57be3d8890fb46babe0
Chris Cordle <ccordle@ufl.edu>
2013-01-29 22:16:07 -0500

    helloworld

---
+++ bin/Debug/AppX/pages/groupDetail/groupDetail.js
@@ -0,0 +1,122 @@
+﻿(function () {
+    "use strict";
+
+    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+    var ui = WinJS.UI;
+
+    function Subcat(obj, groupKey) {
+        return {
+            title: obj.name,
+            subtitle: "Item Subtitle: 1",
+            description: "blahblah",
+            content: "contentcontent",
+            backgroundImage: obj.image_url,
+            groupKey: groupKey,
+            id: obj.id,
+            type: obj.type
+        }
+    }
+
+//    { group: sampleGroups[0], title: "Car Insurance", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: "/images/category/81.jpg" }
+
+    function Item() {
+        return {
+
+        }
+    }
+
+    ui.Pages.define("/pages/groupDetail/groupDetail.html", {
+        /// <field type="WinJS.Binding.List" />
+        _items: null,
+
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            var listView = element.querySelector(".itemslist").winControl;
+            var group;
+            if (options.item)
+                group = options.item;
+            else
+                group = (options && options.groupKey) ? Data.resolveGroupReference(options.groupKey) : Data.groups.getAt(0);
+            this._items = new WinJS.Binding.List();
+
+            var $this = this;
+            if (group.ncategories) {
+                $.get("http://www.giftcertificatesandmore.com/api/v1/category/list/?category_id=" + group.id, function (subcats) {
+                    for (var i = 0; i < subcats.data.length; i++) {
+                        $this._items.push(new Subcat(subcats.data[i], group.key));
+                    }
+                });
+            } else {
+                $.get("http://www.giftcertificatesandmore.com/api/v1/deal/list/?category_id=" + group.id, function (subcats) {
+                    for (var i = 0; i < subcats.data.length; i++) {
+                        $this._items.push(new Subcat(subcats.data[i], group.groupKey));
+                    }
+                });
+            }
+            
+            //this._items = Data.getItemsFromGroup(group);
+            var pageList = this._items.createGrouped(
+                function groupKeySelector(item) {
+                    return group.key ? group.key : group.groupKey;
+                },
+                function groupDataSelector(item) { return group; }
+            );
+
+            element.querySelector("header[role=banner] .pagetitle").textContent = group.title;
+
+            listView.itemDataSource = pageList.dataSource;
+            listView.itemTemplate = options.item ? element.querySelector(".itemtemplate") : element.querySelector(".subcat");
+            listView.groupDataSource = pageList.groups.dataSource;
+            listView.groupHeaderTemplate = element.querySelector(".headertemplate");
+            listView.oniteminvoked = this._itemInvoked.bind(this);
+
+            this._initializeLayout(listView, Windows.UI.ViewManagement.ApplicationView.value);
+            listView.element.focus();
+        },
+
+        unload: function () {
+            //this._items.dispose();
+        },
+
+        // This function updates the page layout in response to viewState changes.
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            var listView = element.querySelector(".itemslist").winControl;
+            if (lastViewState !== viewState) {
+                if (lastViewState === appViewState.snapped || viewState === appViewState.snapped) {
+                    var handler = function (e) {
+                        listView.removeEventListener("contentanimating", handler, false);
+                        e.preventDefault();
+                    }
+                    listView.addEventListener("contentanimating", handler, false);
+                    var firstVisible = listView.indexOfFirstVisible;
+                    this._initializeLayout(listView, viewState);
+                    if (firstVisible >= 0 && listView.itemDataSource.list.length > 0) {
+                        listView.indexOfFirstVisible = firstVisible;
+                    }
+                }
+            }
+        },
+
+        // This function updates the ListView with new layouts
+        _initializeLayout: function (listView, viewState) {
+            /// <param name="listView" value="WinJS.UI.ListView.prototype" />
+
+            if (viewState === appViewState.snapped) {
+                listView.layout = new ui.ListLayout();
+            } else {
+                listView.layout = new ui.GridLayout({ groupHeaderPosition: "left" });
+            }
+        },
+
+        _itemInvoked: function (args) {
+            var item = this._items.getAt(args.detail.itemIndex);
+            if (item.type === "category")
+                WinJS.Navigation.navigate("/pages/groupDetail/groupDetail.html", { item: item });
+            else if (item.type === "deal")
+                WinJS.Navigation.navigate("/pages/itemDetail/itemDetail.html", { item: item });
+        }
+    });
+})();
---
+++ js/data.js
@@ -0,0 +1,138 @@
+﻿(function () {
+    "use strict";
+
+    var list = new WinJS.Binding.List();
+    //var groupedItems = list.createGrouped(
+    //    function groupKeySelector(item) { return item.group.key; },
+    //    function groupDataSelector(item) { return item.group; }
+    //);
+        
+    var categories;
+    var businesses;
+
+    $.get("http://www.giftcertificatesandmore.com/api/v1/category/list/", function (data) {
+        categories = data.data;
+
+        generateSampleData(categories).forEach(function (item) {
+            list.push(item);
+        });
+    });
+
+    $.get("http://www.giftcertificatesandmore.com/api/v1/vendor/list/", function (data) {
+        //populate business data
+        businesses = data.data;
+
+        
+    });
+
+    
+    window.makeAuth = function (user, password) {
+        var tok = user + ':' + password;
+        var hash = Base64.encode(tok);
+        return "Basic " + hash;
+    }
+
+    
+    var url = 'https://www.giftcertificatesandmore.com/api/v1/user/login/';
+
+    // jQuery
+    $.ajax({
+        url: url,
+        method: 'GET',
+        beforeSend: function (req) {
+            var auth = makeAuth();
+            req.setRequestHeader('Authorization', auth);
+        },
+        success: function () {
+            //do this
+        },
+        error: function () {
+            //do this
+        }
+    });
+
+    WinJS.Namespace.define("Data", {
+        groups: list,
+        getItemReference: getItemReference,
+        getItemsFromGroup: getItemsFromGroup,
+        resolveGroupReference: resolveGroupReference,
+        resolveItemReference: resolveItemReference
+    });
+
+    // Get a reference for an item, using the group key and item title as a
+    // unique reference to the item that can be easily serialized.
+    function getItemReference(item) {
+        return [item.group.key, item.title];
+    }
+
+    // This function returns a WinJS.Binding.List containing only the items
+    // that belong to the provided group.
+    function getItemsFromGroup(group) {
+        return list.createFiltered(function (item) { return item.group.key === group.key; });
+    }
+
+    // Get the unique group corresponding to the provided group key.
+    function resolveGroupReference(key) {
+        for (var i = 0; i < list.length; i++) {
+            if (list.getAt(i).key === key) {
+                return list.getAt(i);
+            }
+        }
+    }
+
+    // Get a unique item from the provided string array, which should contain a
+    // group key and an item title.
+    function resolveItemReference(reference) {
+        for (var i = 0; i < groupedItems.length; i++) {
+            var item = groupedItems.getAt(i);
+            if (item.group.key === reference[0] && item.title === reference[1]) {
+                return item;
+            }
+        }
+    }
+
+    // Returns an array of sample data that can be added to the application's
+    // data list. 
+    function generateSampleData(categories) {
+        var itemContent = "<h1>$5 Gift Certificate</h3><p>$5 Gift Certificate valid toward Large & XLarge pizza</p><small>Not valid with any other offer, discount, or coupon</small><p>REDEEM THIS DEAL</p>";
+        var itemDescription = "Item Description: Pellentesque porta mauris quis interdum vehicula urna sapien ultrices velit nec venenatis dui odio in augue cras posuere enim a cursus convallis neque turpis malesuada erat ut adipiscing neque tortor ac erat";
+        var groupDescription = "Group Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor scelerisque lorem in vehicula. Aliquam tincidunt, lacus ut sagittis tristique, turpis massa volutpat augue, eu rutrum ligula ante a ante";
+
+        // These three strings encode placeholder images. You will want to set the
+        // backgroundImage property in your real data to be URLs to images.
+        var darkGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY3B0cPoPAANMAcOba1BlAAAAAElFTkSuQmCC";
+        var lightGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY7h4+cp/AAhpA3h+ANDKAAAAAElFTkSuQmCC";
+        var mediumGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY5g8dcZ/AAY/AsAlWFQ+AAAAAElFTkSuQmCC";
+
+        function Group(group, index) {
+            return {
+                key: index + "group",
+                title: group.name,
+                subtitle: "subtitle here",
+                backgroundImage: group.image_url,
+                description: groupDescription,
+                id: group.id,
+                ncategories: group.ncategories
+            };
+        }
+
+        var sampleGroups = [];
+
+        var groupIndex = 0;
+
+        for (var item in categories) {
+            if (categories[item].parent_id == 0) {
+                sampleGroups.push( new Group(categories[item], groupIndex) );
+                groupIndex++;
+            }
+        }
+
+        // Each of these sample items should have a reference to a particular
+        // group.
+        //var sampleItems = [
+        //    { group: sampleGroups[0], title: "Car Insurance", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: "/images/category/81.jpg" },
+      
+
+        return sampleGroups;
+    }
+})();
